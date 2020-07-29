d642b71aab8aeeca1ebf523bb8589327f2dcbec2
Zsolt Bölöny <bolony.zsolt@gmail.com>
2020-07-29 14:34:52 +0200

    Add deserialize example

---
+++ examples/deserialize.rs
@@ -0,0 +1,11 @@
+use rosm_geostats::*;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let census_json = include_str!("census.json");
+
+    let tilestats: Tilestats = serde_json::from_str(census_json)?;
+
+    println!("{:?}", tilestats);
+
+    Ok(())
+}
