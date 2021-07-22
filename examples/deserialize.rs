a0fb21b087a4c3e94b953a6c51471c221294cd7c
Zsolt Bölöny <bolony.zsolt@gmail.com>
2021-07-22 20:06:30 +0200

    Use pretty printing in the example

--- examples/deserialize.rs
+++ examples/deserialize.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let tilestats: Tilestats = serde_json::from_str(census_json)?;
 
-    println!("{:?}", tilestats);
+    println!("{:#?}", tilestats);
 
     Ok(())
 }
